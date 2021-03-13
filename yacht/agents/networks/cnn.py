47cba2a6d237211778b21498b67a38f4c2ff86e3
IusztinPaul <p.e.iusztin@gmail.com>
2021-03-13 16:30:11 +0200

    Finished EIIECNN module.

--- yacht/agents/networks/cnn.py
+++ yacht/agents/networks/cnn.py
@@ -19,47 +19,4 @@ from torch import nn
 from agents.networks import layers
 
 
-class EIIECNN(nn.Module):
-    def __init__(self, name='EIIE_CNN'):
-        super().__init__()
-        self.name = name
 
-        self.conv_2d = nn.Conv2d(
-            in_channels=3,
-            out_channels=3,
-            kernel_size=(1, 2),
-            stride=(1, 1),
-            padding=(0, 0)
-        )
-        self.eiie_dense = layers.EIIEDense(
-            input_channels=3,
-            output_channels=10,
-            kernel_width=30
-        )
-        self.eiie_output_with_w = layers.EIIEOutputWithW(
-            input_channels=11
-        )
-
-    def forward(self, input_tensor, previous_w):
-        batch, features, assets, window = input_tensor.shape
-
-        last_window_item_closing_prices = input_tensor[:, 0, :, -1]
-        last_window_item_closing_prices = last_window_item_closing_prices.reshape(batch, 1, assets, 1)
-
-        tensor = input_tensor / last_window_item_closing_prices
-
-        tensor = self.conv_2d(tensor)
-        tensor = self.eiie_dense(tensor)
-        tensor = self.eiie_output_with_w(tensor, previous_w)
-
-        return tensor
-
-
-if __name__ == '__main__':
-    eiie_network = EIIECNN().to('cuda')
-
-    result = eiie_network(
-        torch.ones((1, 3, 11, 31)).to('cuda'),
-        torch.ones((1, 11)).to('cuda')
-    )
-    print(result)
--- yacht/agents/networks/layers/cnn.py
+++ yacht/agents/networks/layers/cnn.py
@@ -30,14 +30,14 @@ class EIIEDense(nn.Module):
 class EIIEOutputWithW(nn.Module):
     def __init__(
             self,
-            input_channels,
+            num_assets: int,
             name='EIIEOutputWithW'
     ):
         super().__init__()
         self.name = name
 
         self.conv_2d = nn.Conv2d(
-            input_channels,
+            num_assets,
             1,
             kernel_size=(1, 1),
             stride=(1, 1),
@@ -47,10 +47,10 @@ class EIIEOutputWithW(nn.Module):
     def forward(self, input_tensor, previous_w):
         batch, features, num_assets, width = input_tensor.shape
 
-        input_tensor = input_tensor.reshape(batch, num_assets, 1, width * features)
-        previous_w = previous_w.reshape(batch, num_assets, 1, 1)
+        input_tensor = input_tensor.reshape(batch, width * features, num_assets, 1)
+        previous_w = previous_w.reshape(batch, 1, num_assets, 1)
 
-        tensor = torch.cat([input_tensor, previous_w], dim=3)
+        tensor = torch.cat([input_tensor, previous_w], dim=1)
         tensor = self.conv_2d(tensor)
         tensor = tensor.reshape(batch, num_assets)
 
@@ -61,3 +61,65 @@ class EIIEOutputWithW(nn.Module):
         tensor = F.softmax(tensor)
 
         return tensor
+
+
+class EIIECNN(nn.Module):
+    def __init__(
+            self,
+            input_num_features: int,
+            num_assets: int,
+            window_size: int,
+            kernel_size=(1, 2),
+            name='EIIE_CNN'
+    ):
+        super().__init__()
+        self.name = name
+
+        self.conv_2d = nn.Conv2d(
+            in_channels=input_num_features,
+            out_channels=input_num_features,
+            kernel_size=kernel_size,
+            stride=(1, 1),
+            padding=(0, 0)
+        )
+        self.eiie_dense = EIIEDense(
+            input_channels=input_num_features,
+            output_channels=num_assets - 1,
+            kernel_width=window_size - kernel_size[1] + 1
+        )
+        self.eiie_output_with_w = EIIEOutputWithW(
+            num_assets=num_assets
+        )
+
+    def forward(self, input_tensor, previous_w):
+        batch, features, assets, window = input_tensor.shape
+
+        last_window_assets_closing_price = input_tensor[:, 0, :, -1]
+        last_window_assets_closing_price = last_window_assets_closing_price.reshape(batch, 1, assets, 1)
+
+        tensor = input_tensor / last_window_assets_closing_price
+
+        tensor = self.conv_2d(tensor)
+        tensor = self.eiie_dense(tensor)
+        tensor = self.eiie_output_with_w(tensor, previous_w)
+
+        return tensor
+
+
+if __name__ == '__main__':
+    BATCH_SIZE = 1
+    NUM_FEATURES = 3
+    NUM_ASSETS = 11
+    WINDOW_SIZE = 31
+
+    eiie_network = EIIECNN(
+        input_num_features=NUM_FEATURES,
+        num_assets=NUM_ASSETS,
+        window_size=WINDOW_SIZE,
+    ).to('cuda')
+
+    result = eiie_network(
+        torch.ones((BATCH_SIZE, NUM_FEATURES, NUM_ASSETS, WINDOW_SIZE)).to('cuda'),
+        torch.ones((BATCH_SIZE, NUM_ASSETS)).to('cuda')
+    )
+    print(result)
