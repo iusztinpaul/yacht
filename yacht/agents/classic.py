f77a5cd772013d4a4cd89f6861430bed9ef44a8f
IusztinPaul <p.e.iusztin@gmail.com>
2021-09-11 15:21:33 +0300

    Finished classic agents.

--- yacht/agents/classic.py
+++ yacht/agents/classic.py
@@ -9,13 +9,29 @@ class BaseClassicAgent(ABC):
     def __init__(self, env: VecEnv):
         self.env = env
 
-    @abstractmethod
     def predict(
             self,
             observation: Union[Dict[str, np.ndarray], np.ndarray],
             state: Optional[np.ndarray] = None,
             deterministic: bool = False,
     ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+        if state is not None and (state == 0).any():
+            self.reset()
+
+        actions = self._predict(observation, deterministic)
+
+        return actions, np.ones_like(actions)
+
+    @abstractmethod
+    def _predict(
+            self,
+            observation: Union[Dict[str, np.ndarray], np.ndarray],
+            deterministic: bool = False,
+    ) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def reset(self) -> None:
         pass
 
 
@@ -25,23 +41,18 @@ class BuyAndHoldAgent(BaseClassicAgent):
 
         self.bought = False
 
-    def predict(
+    def _predict(
             self,
             observation: Union[Dict[str, np.ndarray], np.ndarray],
-            state: Optional[np.ndarray] = None,
             deterministic: bool = False,
     ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
         num_envs = observation['1d'].shape[0]
         num_assets = observation['1d'].shape[3]
 
-        if state is not None and (state == 0).any():
-            self.bought = False
-
         if self.bought is False:
             total_close_prices = observation['1d'][:, -1, 0, :, 0]
             total_cash_positions = observation['env_features'][:, -1, 0]
 
-            # TODO: Adapt cash distribution by market cap.
             total_cash_per_asset = total_cash_positions / num_assets
             total_cash_per_asset = np.tile(total_cash_per_asset, reps=num_assets)
             total_cash_per_asset = total_cash_per_asset.reshape(num_envs, num_assets)
@@ -51,7 +62,10 @@ class BuyAndHoldAgent(BaseClassicAgent):
         else:
             actions = np.zeros(shape=(num_envs, num_assets))
 
-        return actions, np.ones(shape=(num_envs, num_assets))
+        return actions
+
+    def reset(self) -> None:
+        self.bought = False
 
 
 class DCFAgent(BaseClassicAgent):
@@ -61,37 +75,29 @@ class DCFAgent(BaseClassicAgent):
         self.start_tick = self.env.envs[0].unwrapped.start_tick
         self.end_tick = self.env.envs[0].unwrapped.end_tick
         self.current_tick = self.start_tick
+
         self.num_buying_times = 4
         self.buy_period = (self.end_tick - self.start_tick) // self.num_buying_times
         self.buying_ticks = [v for v in range(self.start_tick, self.end_tick, self.buy_period)]
         self.bought_n_times = 0
+
         self.cash_distribution_per_tick = None
         self.cash_distribution_per_ticker = None
 
-    def predict(
+    def _predict(
             self,
             observation: Union[Dict[str, np.ndarray], np.ndarray],
-            state: Optional[np.ndarray] = None,
             deterministic: bool = False,
-    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+    ) -> np.ndarray:
         num_envs = observation['1d'].shape[0]
         num_assets = observation['1d'].shape[3]
-
-        if state is not None and (state == 0).any():
-            assert self.bought_n_times == self.num_buying_times
-
-            self.cash_distribution_per_ticker = None
-            self.bought_n_times = 0
-            self.current_tick = self.start_tick
 
         if self.cash_distribution_per_ticker is None:
             total_cash_positions = observation['env_features'][:, -1, 0]
             self.cash_distribution_per_tick = total_cash_positions / self.num_buying_times
 
-            # TODO: Adapt cash distribution by market cap.
-            num_assets = observation['1d'].shape[3]
             self.cash_distribution_per_ticker = self.cash_distribution_per_tick / num_assets
-            self.cash_distribution_per_ticker = np.tile(self.cash_distribution_per_ticker, num_assets)
+            self.cash_distribution_per_ticker = self.cash_distribution_per_ticker.reshape(num_envs, 1)
 
         if self.bought_n_times < self.num_buying_times and self.current_tick >= self.buying_ticks[self.bought_n_times]:
             total_close_prices = observation['1d'][:, -1, 0, :, 0]
@@ -103,4 +109,11 @@ class DCFAgent(BaseClassicAgent):
 
         self.current_tick += 1
 
-        return actions, np.ones(shape=(num_envs, num_assets))
+        return actions
+
+    def reset(self) -> None:
+        assert self.bought_n_times == self.num_buying_times
+
+        self.cash_distribution_per_ticker = None
+        self.bought_n_times = 0
+        self.current_tick = self.start_tick
